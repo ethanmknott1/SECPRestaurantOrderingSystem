e6d7e5ecf618a9f19332058038bf68e1d9ce252c
o-bickel <bick7185@kettering.edu>
2022-03-15 14:00:40 -0400

    Renamed buttons, added some comments

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -15,9 +15,10 @@ import orderflow.Order;
 public class GUI implements ActionListener {
     static Order order;
 
-    // global GUI objects (so they may be defined in constructor and referenced via user action)
+    // global GUI objects (so they may be defined in constructor and referenced via ActionListener)
     JFrame mainFrame;
     
+    // panel components for housing various layouts and other components (JButton, JList)
     JPanel mainPanel;
     JPanel headersPanel;
     JPanel centerPanel;
@@ -26,82 +27,103 @@ public class GUI implements ActionListener {
     JPanel summaryPanel;
     JPanel orderEditPanel;
 
+    // panel specifically used for creating visual spacing in the mainFrame BorderLayout
     JPanel spacing0;
-    JPanel spacing1;
-    JPanel spacing2;
-    JPanel spacing3;
-    JPanel spacing4;
+
+    // various JButtons for menu items and order options
+    JButton btn_burger;
+    JButton btn_double_burger;
+    JButton btn_crispy_sandwich;
+    JButton btn_grilled_sandwich;
+    JButton btn_fish_sandwich;
+    JButton btn_nuggets;
+    JButton btn_pep_pizza;
+    JButton btn_meat_pizza;
+    JButton btn_fries;
+    JButton btn_tots;
+    JButton btn_hashbrowns;
+    JButton btn_onion_rings;
+    JButton btn_water;
+    JButton btn_cola;
+    JButton btn_coffee;
+    JButton btn_juice;
+    JButton btn_item_options;
+    JButton btn_delete_item;
+    JButton btn_complete_order;
 
     public GUI(){
-        JButton b1 = new JButton("<html>Single Cheeseburger<br />Side: Fries</html>");
-        JButton b2 = new JButton("<html>Double Cheeseburger<br />Side: Fries</html>");
-        JButton b3 = new JButton("<html>Crispy Chicken Sandwich<br />Side: Fries</html>");
-        JButton b4 = new JButton("<html>Grilled Chicken Sandwich<br />Side: Fries</html>");
-        JButton b5 = new JButton("<html>Fish Fillet Sandwich<br />Side: Tater Tots</html>");
-        JButton b6 = new JButton("<html>10pc Chicken Nuggets<br />Side: Fries</html>");
-        JButton b7 = new JButton("<html>Pepperoni Pizza<br />Side: Fries</html>");
-        JButton b8 = new JButton("<html>Meat Lovers Pizza<br />Side: Fries</html>");
-        JButton b9 = new JButton("French Fries");
-        JButton b10 = new JButton("Tater Tots");
-        JButton b11 = new JButton("Hashbrowns");
-        JButton b12 = new JButton("Onion Rings");
-        JButton b13 = new JButton("Water");
-        JButton b14 = new JButton("Cola");
-        JButton b15 = new JButton("Coffee");
-        JButton b16 = new JButton("Juice");
-        JButton b17 = new JButton("Item Options");
-        JButton b18 = new JButton("Delete Item");
-        JButton b19 = new JButton("Complete Order");
+        // create relevant buttons for the menu
+        btn_burger = new JButton("<html>Single Cheeseburger<br />Side: Fries</html>");
+        btn_double_burger = new JButton("<html>Double Cheeseburger<br />Side: Fries</html>");
+        btn_crispy_sandwich = new JButton("<html>Crispy Chicken Sandwich<br />Side: Fries</html>");
+        btn_grilled_sandwich = new JButton("<html>Grilled Chicken Sandwich<br />Side: Fries</html>");
+        btn_fish_sandwich = new JButton("<html>Fish Fillet Sandwich<br />Side: Tater Tots</html>");
+        btn_nuggets = new JButton("<html>10pc Chicken Nuggets<br />Side: Fries</html>");
+        btn_pep_pizza = new JButton("<html>Pepperoni Pizza<br />Side: Fries</html>");
+        btn_meat_pizza = new JButton("<html>Meat Lovers Pizza<br />Side: Fries</html>");
+        btn_fries = new JButton("French Fries");
+        btn_tots = new JButton("Tater Tots");
+        btn_hashbrowns = new JButton("Hashbrowns");
+        btn_onion_rings = new JButton("Onion Rings");
+        btn_water = new JButton("Water");
+        btn_cola = new JButton("Cola");
+        btn_coffee = new JButton("Coffee");
+        btn_juice = new JButton("Juice");
+        btn_item_options = new JButton("Item Options");
+        btn_delete_item = new JButton("Delete Item");
+        btn_complete_order = new JButton("Complete Order");
 
-        // define GUI objects
+        // Main Frame and Panel
         mainFrame = new JFrame();
         
         mainPanel = new JPanel();
         BorderLayout layout = new BorderLayout();
         layout.setHgap(100);
         layout.setVgap(50);
-        //GridLayout layout = new GridLayout(1, 9);
-        //layout.setHgap(10);
-        //layout.setVgap(10);
         mainPanel.setLayout(layout);
 
+        // Headers Panel
         headersPanel = new JPanel();
         JLabel headers = new JLabel("<html><h1>Meals&emsp;&emsp;&emsp;A La Carte&emsp;&emsp;&emsp;Summary</h1></html>");
         headersPanel.add(headers);
 
+        // Center Panel that houses mealsPanel, aLaCartePanel, summaryPanel, and orderEditPanel
         centerPanel = new JPanel();
         GridLayout centerLayout = new GridLayout(1, 0);
         centerLayout.setHgap(50);
         centerPanel.setLayout(centerLayout);
 
+        // Meals Panel layout
         mealsPanel = new JPanel();
         GridLayout mealsLayout = new GridLayout(0, 2);
         mealsLayout.setHgap(10);
         mealsLayout.setVgap(10);
         mealsPanel.setLayout(mealsLayout);
-        mealsPanel.add(b1);
-        mealsPanel.add(b2);
-        mealsPanel.add(b3);
-        mealsPanel.add(b4);
-        mealsPanel.add(b5);
-        mealsPanel.add(b6);
-        mealsPanel.add(b7);
-        mealsPanel.add(b8);
+        mealsPanel.add(btn_burger);
+        mealsPanel.add(btn_double_burger);
+        mealsPanel.add(btn_crispy_sandwich);
+        mealsPanel.add(btn_grilled_sandwich);
+        mealsPanel.add(btn_fish_sandwich);
+        mealsPanel.add(btn_nuggets);
+        mealsPanel.add(btn_pep_pizza);
+        mealsPanel.add(btn_meat_pizza);
 
+        // A La Carte Panel layout
         aLaCartePanel = new JPanel();
         GridLayout aLaCarteLayout = new GridLayout(0, 2);
         aLaCarteLayout.setHgap(10);
         aLaCarteLayout.setVgap(10);
         aLaCartePanel.setLayout(aLaCarteLayout);
-        aLaCartePanel.add(b9);
-        aLaCartePanel.add(b10);
-        aLaCartePanel.add(b11);
-        aLaCartePanel.add(b12);
-        aLaCartePanel.add(b13);
-        aLaCartePanel.add(b14);
-        aLaCartePanel.add(b15);
-        aLaCartePanel.add(b16);
+        aLaCartePanel.add(btn_fries);
+        aLaCartePanel.add(btn_tots);
+        aLaCartePanel.add(btn_hashbrowns);
+        aLaCartePanel.add(btn_onion_rings);
+        aLaCartePanel.add(btn_water);
+        aLaCartePanel.add(btn_cola);
+        aLaCartePanel.add(btn_coffee);
+        aLaCartePanel.add(btn_juice);
 
+        // Summary Panel layout
         summaryPanel = new JPanel();
         JList<FoodItem> summaryList = new JList<FoodItem>();
         summaryList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
@@ -112,33 +134,38 @@ public class GUI implements ActionListener {
         listScroller.setPreferredSize(new Dimension(10, 10));
         summaryPanel.add(summaryList);
 
+        // Order Edit Panel layout
         orderEditPanel = new JPanel();
         GridLayout orderEditLayout = new GridLayout(3, 1);
         orderEditLayout.setHgap(10);
         orderEditLayout.setVgap(80); 
         orderEditPanel.setLayout(orderEditLayout);
-        orderEditPanel.add(b17);
-        orderEditPanel.add(b18);
-        orderEditPanel.add(b19);
+        orderEditPanel.add(btn_item_options);
+        orderEditPanel.add(btn_delete_item);
+        orderEditPanel.add(btn_complete_order);
 
+        // Add panels to the Center Panel to create adequate horizontal spacing
         centerPanel.add(mealsPanel);
         centerPanel.add(aLaCartePanel);
         centerPanel.add(summaryPanel);
         centerPanel.add(orderEditPanel);
 
+        // Spacing Panel
         spacing0 = new JPanel();
         spacing0.setPreferredSize(new Dimension(10, 170));
 
+        // Add all relevant panels to the Main Panel
         mainPanel.add(headersPanel, BorderLayout.NORTH);
         mainPanel.add(centerPanel, BorderLayout.CENTER);
         mainPanel.add(spacing0, BorderLayout.SOUTH);
         
+        // Further define Main Frame layout and functionality
         mainFrame.add(mainPanel, BorderLayout.CENTER);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setTitle("Ordering System");
-        
-        mainFrame.pack();
         mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        mainFrame.pack();
         mainFrame.setVisible(true);
     }
 
@@ -157,6 +184,6 @@ public class GUI implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e){
-
+        
     }
 }
